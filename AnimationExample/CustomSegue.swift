ae9c558d48b9beb1816abf81c160c5f92b4bffd6
Enovaid <180103167@stu.sdu.edu.kz>
2020-12-09 06:47:26 +0600

    Animation Example

---
+++ AnimationExample/CustomSegue.swift
@@ -0,0 +1,28 @@
+//
+//  CustomSegue.swift
+//  AnimationExample
+//
+//  Created by Айдана on 11/15/20.
+//
+
+import UIKit
+
+class CustomSegue: UIStoryboardSegue {
+    override func perform() {
+        let destination_view = self.destination.view
+        let source_view = self.source.view
+        UIView.animate(withDuration: 2, animations: {
+            // animating
+            source_view?.alpha = 0
+        }) { (Bool) in
+            destination_view?.alpha = 0
+            self.source.present(self.destination, animated: false) {
+                UIView.animate(withDuration: 2) {
+                    destination_view?.alpha = 1
+                }
+            }
+            // completing
+        }
+    }
+
+}
--- AnimationExample/ViewController.swift
+++ AnimationExample/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var target_image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func animatePressed(_ sender: UIButton) {
+        switch sender.currentTitle {
+        case "Fade out":
+            UIView.animate(withDuration: 1) { [self] in
+                target_image.alpha = 0
+            }
+        case "Fade in":
+            UIView.animate(withDuration: 1) { [self] in
+                target_image.alpha = 1
+            }
+        case "Move":
+            UIView.animate(withDuration: 1) { [self] in
+                target_image.center = CGPoint(x: 0, y: 0)
+            }
+        case "Back":
+            UIView.animate(withDuration: 1) { [self] in
+                target_image.center = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height/2)
+            }
+        case "Zoom in":
+            UIView.animate(withDuration: 1) { [self] in
+                target_image.transform = CGAffineTransform(scaleX: 2, y: 2)
+            }
+        case "Zoom out":
+            UIView.animate(withDuration: 1) { [self] in
+                target_image.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }
+        default:
+            break
+        }
+    }
+    
 }
 
